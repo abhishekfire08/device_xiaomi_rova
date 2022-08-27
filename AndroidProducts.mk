16b77a3fe9a39095228730840737b0290b704059
Abhishek Hembrom <76091481+abhishekhembrom08@users.noreply.github.com>
2022-08-27 23:29:43 +0530

    Update AndroidProducts.mk
    nad to bootleg

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/nad_rova.mk
+    $(LOCAL_DIR)/bootleg_rova.mk
 
 COMMON_LUNCH_CHOICES := \
-    nad_rova-user \
-    nad_rova-userdebug \
-    nad_rova-eng
+    bootleg_rova-user \
+    bootleg_rova-userdebug \
+    bootleg_rova-eng
