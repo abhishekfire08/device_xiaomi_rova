a17058898d1b9485c53eafe9952628bc4df6b4ff
Abhishek Hembrom <76091481+abhishekhembrom08@users.noreply.github.com>
2022-09-10 20:32:27 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -182,10 +182,6 @@ PRODUCT_PACKAGES += \
     libhwbinder \
     libhwbinder.vendor
 
-# HW crypto
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
-
 # IMS
 PRODUCT_PACKAGES += \
     libshim_imsvideocodec \
