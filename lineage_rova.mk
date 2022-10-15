75b00331405953414a0ab97b509dd10528f72b46
Abhishek Hembrom <76091481+abhishekhembrom08@users.noreply.github.com>
2022-10-15 08:32:14 +0530

    Update and rename evolution_rova.mk to lineage_rova.mk

--- lineage_rova.mk
+++ lineage_rova.mk
@@ -13,14 +13,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk
 TARGET_BOOT_ANIMATION_RES := 720
 
 # Inherit some common evolution stuff
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from rova device
 $(call inherit-product, device/xiaomi/rova/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := rova
-PRODUCT_NAME := evolution_rova
+PRODUCT_NAME := lineage_rova
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 4A / 5A
