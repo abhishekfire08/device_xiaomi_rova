bfa5a63ef0c8706439f5f08a25aed2591b72a467
abhishekfre08 <abhishekhembrom08@gmail.com>
2022-12-16 08:01:09 +0530

    Revert "rova: nuked clang path."
    This reverts commit 3784e58dd75c93a23ae1def11735ff7e12ec753e.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -110,20 +110,22 @@ DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_rova
 TARGET_RECOVERY_DEVICE_MODULES := libinit_rova
 
-# Kernel 
- BOARD_KERNEL_BASE := 0x80000000 
- BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci 
- BOARD_KERNEL_CMDLINE += earlycon=msm_hsl_uart,0x78b000 
- BOARD_KERNEL_CMDLINE += androidboot.usbconfigfs=true 
- BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb 
- BOARD_KERNEL_PAGESIZE := 2048 
- BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100 
- TARGET_KERNEL_SOURCE := kernel/xiaomi/rova 
- TARGET_KERNEL_CONFIG := mi8937_defconfig 
- TARGET_KERNEL_VERSION := 4.9 
- TARGET_KERNEL_CLANG_COMPILE := true 
- TARGET_KERNEL_ADDITIONAL_FLAGS := LLVM=1 \ 
-     HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"-line-argument"
+# Kernel
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci
+BOARD_KERNEL_CMDLINE += earlycon=msm_hsl_uart,0x78b000
+BOARD_KERNEL_CMDLINE += androidboot.usbconfigfs=true
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
+TARGET_KERNEL_SOURCE := kernel/xiaomi/rova
+TARGET_KERNEL_CONFIG := mi8937_defconfig
+TARGET_KERNEL_VERSION := 4.9
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_VERSION := clang-r450784d
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-$(TARGET_KERNEL_CLANG_VERSION)
+TARGET_KERNEL_ADDITIONAL_FLAGS := LLVM=1 \
+    HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 
 # Media
 USE_DEVICE_SPECIFIC_MEDIA := true
